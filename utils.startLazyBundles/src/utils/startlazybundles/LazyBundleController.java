656726bfa876b54ff23dacb0acbd090ab38b35e8
Christian Baranowski <develop@pro-pix.de>
2012-08-11 17:21:23 +0200

    - add activator class now with a new name the activator is called controller and can be used from bnd and in a normal OSGi environment.

---
+++ utils.startLazyBundles/src/utils/startlazybundles/LazyBundleController.java
@@ -0,0 +1,20 @@
+package utils.startlazybundles;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class LazyBundleController implements BundleActivator {
+	
+	LazyBundlesActivation activation = new LazyBundlesActivation();
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		activation.start(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		activation.stop();
+	}
+
+}
