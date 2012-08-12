e2a56bfc8207f04ea397e7762026df55015b1130
Christian Baranowski <tux2323@pro-pix.de>
2012-08-12 10:23:02 +0200

    - Update the logging msg in the test bundle c

--- utils.startLazyBundles.test.bundles/src/bundle/c/Activator.java
+++ utils.startLazyBundles.test.bundles/src/bundle/c/Activator.java
@@ -7,12 +7,12 @@ public class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Start Bundle C");
+		System.err.println("The bundle C should never be started");
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		System.out.println("Stop Bundle C");
+		System.err.println("The bundle C should never be stopped");
 	}
 
 }
