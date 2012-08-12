2e127850ebcfea26c1e5a59f42d7118b94abe98b
Christian Baranowski <tux2323@pro-pix.de>
2012-08-12 12:06:51 +0200

    - Clean up the integration tests.

--- utils.startLazyBundles.test/src/utils/startlazybundles/tests/LazyBundleControllerTest.java
+++ utils.startLazyBundles.test/src/utils/startlazybundles/tests/LazyBundleControllerTest.java
@@ -10,42 +10,48 @@ import org.osgi.framework.FrameworkUtil;
 import junit.framework.TestCase;
 
 public class LazyBundleControllerTest extends TestCase {
-	
-	final static BundleContext bundleContext = 
-			FrameworkUtil.getBundle(LazyBundleControllerTest.class).getBundleContext();
-	
+
+	final static BundleContext bundleContext = FrameworkUtil.getBundle(
+			LazyBundleControllerTest.class).getBundleContext();
+
+	final static File testBundleFolder = new File(
+			"../utils.startLazyBundles.test.bundles/generated/");
+
 	public void testBundleAStateIsActive() throws Exception {
 		assertNotNull(bundleContext);
 		Bundle bundleA = findBundle("utils.startLazyBundles.test.bundles.a");
 		assertEquals(Bundle.ACTIVE, bundleA.getState());
 	}
-	
+
 	public void testInstallAndStartLazyBundleB() throws Exception {
-		String fileName = "../utils.startLazyBundles.test.bundles/generated/utils.startLazyBundles.test.bundles.b.jar";
-		File bundleBFile = new File(fileName);
+		File bundleBFile = new File(testBundleFolder,
+				"utils.startLazyBundles.test.bundles.b.jar");
 		assertTrue("Bundle B must exist.", bundleBFile.exists());
-		Bundle bundleB = bundleContext.installBundle("utils.startLazyBundles.test.bundles.b", new FileInputStream(bundleBFile ));
+		Bundle bundleB = bundleContext.installBundle(bundleBFile.getName(),
+				new FileInputStream(bundleBFile));
 		bundleB.start(Bundle.START_ACTIVATION_POLICY);
 		assertEquals(Bundle.ACTIVE, bundleB.getState());
 	}
-	
+
 	public void testInstallAndNotActivateBundleC() throws Exception {
-		String fileName = "../utils.startLazyBundles.test.bundles/generated/utils.startLazyBundles.test.bundles.c.jar";
-		File bundleCFile = new File(fileName);
+		File bundleCFile = new File(testBundleFolder,
+				"utils.startLazyBundles.test.bundles.c.jar");
 		assertTrue("Bundle C must exist.", bundleCFile.exists());
-		Bundle bundleC = bundleContext.installBundle("utils.startLazyBundles.test.bundles.c", new FileInputStream(bundleCFile));
+		Bundle bundleC = bundleContext.installBundle(bundleCFile.getName(),
+				new FileInputStream(bundleCFile));
 		bundleC.start(Bundle.START_ACTIVATION_POLICY);
 		assertEquals(Bundle.STARTING, bundleC.getState());
 	}
-	
-	Bundle findBundle(String symbolicName){
+
+	Bundle findBundle(String symbolicName) {
 		Bundle[] bundles = bundleContext.getBundles();
 		for (Bundle bundle : bundles) {
-			if(bundle.getSymbolicName().equals(symbolicName)){
+			if (bundle.getSymbolicName().equals(symbolicName)) {
 				return bundle;
 			}
 		}
-		throw new RuntimeException("No Bundle found with symbolic name " + symbolicName);
+		throw new RuntimeException("No Bundle found with symbolic name "
+				+ symbolicName);
 	}
-	
+
 }
